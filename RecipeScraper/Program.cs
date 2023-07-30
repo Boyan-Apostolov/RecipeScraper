2f7c4d9387aef2218fe783b7c629b00e3f660a6e
Boyan-Apostolov <58187770+Boyan-Apostolov@users.noreply.github.com>
2023-07-30 12:48:48 +0300

    Parse recipe data

--- RecipeScraper/Program.cs
+++ RecipeScraper/Program.cs
@@ -5,13 +5,29 @@ using Fizzler.Systems.HtmlAgilityPack;
 using HtmlAgilityPack;
 using RestSharp;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, RecipeScraper!");
 
 var baseUrl = "https://recepti.gotvach.bg/";
 var restClient = new RestClient(baseUrl);
 
-var recipes = await GetPageRecipesLinks(2);
-Console.WriteLine(string.Join(Environment.NewLine, recipes));
+var recipesLinks = await GetPageRecipesLinks(2);
+
+var parsedRecipes = new List<Recipe>();
+foreach (var recipeLink in recipesLinks)
+{
+    try
+    {
+        var recipe = await GetRecipeData(recipeLink);
+        parsedRecipes.Add(recipe);
+    }
+    catch (Exception e)
+    {
+        PrintError($"{e.Message}. Problem from recipe: {recipeLink}");
+        PrintError($"{e.StackTrace}");
+    }
+}
+
+;
 
 async Task<HtmlNode> GetDocumentNode(string url)
 {
@@ -21,7 +37,7 @@ async Task<HtmlNode> GetDocumentNode(string url)
 
     var doc = new HtmlDocument();
     doc.LoadHtml(response.Content);
-    
+
     return doc.DocumentNode;
 }
 
@@ -30,14 +46,21 @@ async Task<List<string>> GetPageRecipesLinks(int pages)
     var foundLinks = new List<string>();
     for (int i = 1; i <= pages; i++)
     {
-        var recipesPage = $"{i}?=6";
-        var currentPage = await GetDocumentNode(recipesPage);
-        
-        var currentPageRecipeLinks = currentPage.QuerySelectorAll(".rprev>div") //Recipe block
-            .Select(recipeBlock => recipeBlock.QuerySelector(".title") // Title, containing the url of the recipe
-                .GetAttributeValue("href", ""))
-            .ToList();
-        foundLinks.AddRange(currentPageRecipeLinks);
+        try
+        {
+            var recipesPage = $"{i}?=6";
+            var currentPage = await GetDocumentNode(recipesPage);
+
+            var currentPageRecipeLinks = currentPage.QuerySelectorAll(".rprev>div") //Recipe block
+                .Select(recipeBlock => recipeBlock.QuerySelector(".title") // Title, containing the url of the recipe
+                    .GetAttributeValue("href", ""))
+                .ToList();
+            foundLinks.AddRange(currentPageRecipeLinks);
+        }
+        catch (Exception e)
+        {
+            PrintError(e.Message);
+        }
     }
 
     return foundLinks;
@@ -51,13 +74,98 @@ async Task<Recipe> GetRecipeData(string recipeUrl)
 
     recipe.Url = recipeUrl;
     recipe.Title = currentPage.QuerySelector("h1").InnerText;
-    
+
+    var totalTimeText = currentPage //Format : "125 mins."
+        .QuerySelector(".icb-tot")?
+        .ChildNodes[1]
+        .InnerText
+        .Split(" ")[0];
+    recipe.TotalCookingTime = string.IsNullOrWhiteSpace(totalTimeText)
+        ? null
+        : TimeSpan.FromMinutes(int.Parse(totalTimeText));
+
+    var portionsText = currentPage //Format : "1"
+        .QuerySelector(".icb-fak")?
+        .ChildNodes[1]
+        .InnerText;
+    recipe.Portions = string.IsNullOrWhiteSpace(portionsText) 
+        ? 1 
+        : int.Parse(portionsText);
+
+    recipe.CookingInstructions = currentPage.QuerySelector(".text").InnerText
+        .Replace("Начин на приготвяне", "");
+
+    recipe.Products = new List<Product>();
+    var productItems = currentPage
+        .QuerySelectorAll(".products.new li");
+
+    var lastSubItem = string.Empty;
+    foreach (var productItem in productItems)
+    {
+        var itemsPurpose = productItem.QuerySelector(".sub");
+        if (itemsPurpose != null)
+        {
+            //The recipe has multiple sub-items, the following items until the next .sub are used for the current sub-item
+            lastSubItem = itemsPurpose.InnerText;
+            continue;
+        }
+        else
+        {
+            var productItemName = productItem.QuerySelector("b").InnerText;
+
+            var productItemHasQuantityText = productItem
+                .ChildNodes;
+            
+            var productItemQuantityText = productItemHasQuantityText.Count == 1 
+                ? ""
+                : productItemHasQuantityText[1].InnerText
+                .Split(" - ")[1];
+            recipe.Products.Add(new Product()
+            {
+                Name = productItemName,
+                ProductQuantity = new ProductQuantity() { QuantityText = productItemQuantityText },
+                ProductPurpose = new ProductPurpose() { Name = lastSubItem },
+            });
+        }
+    }
+
     return recipe;
+}
+
+void PrintError(string errorMsg)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(errorMsg);
+    Console.ForegroundColor = ConsoleColor.White;
 }
 
 public class Recipe
 {
     public string Title { get; set; }
-
     public string Url { get; set; }
+    public TimeSpan? TotalCookingTime { get; set; }
+    public int Portions { get; set; }
+
+    public ICollection<Product> Products { get; set; }
+
+    public string CookingInstructions { get; set; }
+}
+
+public class Product
+{
+    public string Name { get; set; }
+
+    public ProductQuantity ProductQuantity { get; set; }
+
+    public ProductPurpose? ProductPurpose { get; set; }
+}
+
+public class ProductQuantity
+{
+    public string QuantityText { get; set; }
+}
+
+public class ProductPurpose
+{
+    public string Name { get; set; }
 }
